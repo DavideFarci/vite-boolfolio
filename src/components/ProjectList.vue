7baf2a21d3d21284ba6f1a4ea625f3ec9a2219e4
DavideFarci <yank92.df@gmail.com>
2023-07-14 15:30:24 +0200

    first commit

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,23 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      arrProjects: [],
+    };
+  },
+  created() {
+    axios.get("").then(response);
+  },
+};
+</script>
+
+<template>
+  <h1 class="text-danger">boolpress</h1>
+  <ul>
+    <li v-for="project in arrProjects" :key="post.id">{{ project.title }}</li>
+  </ul>
+</template>
+
+<style lang="scss" scoped></style>
