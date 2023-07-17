4900b49f05324a19ce2a28f15eb66b1ddf7a5c5a
DavideFarci <yank92.df@gmail.com>
2023-07-17 17:34:01 +0200

    add components content

---
+++ src/components/AppNav.vue
@@ -0,0 +1,29 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <nav>
+    <div class="container">
+      <div class="row">
+        <div class="content">
+          <ul class="d-flex gap-3 list-unstyled">
+            <li>
+              <router-link class="navbar-brand" :to="{ name: 'home' }"
+                >Boolpress</router-link
+              >
+            </li>
+            <li>
+              <router-link class="nav-item" :to="{ name: 'projects.index' }"
+                >Projects</router-link
+              >
+            </li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style></style>
---
+++ src/pages/AppProjects.vue
@@ -0,0 +1,17 @@
+<script>
+import ProjectList from "../components/ProjectList.vue";
+export default {
+  components: {
+    ProjectList,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h2>Projects List</h2>
+    <ProjectList />
+  </div>
+</template>
+
+<style></style>
