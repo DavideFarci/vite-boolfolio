ac64c8ff30999888633a77b0ae875d1449b8e735
DavideFarci <yank92.df@gmail.com>
2023-07-18 15:18:20 +0200

    add search-bar in nav

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -7,7 +7,7 @@ export default {};
     <div class="container">
       <div class="row">
         <div class="content">
-          <ul class="d-flex gap-3 list-unstyled">
+          <ul class="d-flex gap-3 py-4 list-unstyled">
             <li>
               <router-link class="navbar-brand" :to="{ name: 'home' }"
                 >Boolpress</router-link
@@ -23,7 +23,19 @@ export default {};
                 >Projects</router-link
               >
             </li>
-            <li></li>
+            <li>
+              <form class="d-flex" role="search">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button class="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </li>
           </ul>
         </div>
       </div>
