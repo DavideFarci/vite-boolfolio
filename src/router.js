b13376938554545fe7e08d66715cb158de11c894
DavideFarci <yank92.df@gmail.com>
2023-07-18 15:06:18 +0200

    add app 404

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import AppHome from "./pages/AppHome.vue";
 import AppProjects from "./pages/AppProjects.vue";
 import AppShow from "./pages/AppShow.vue";
 import AppAbout from "./pages/AppAbout.vue";
+import App404 from "./pages/App404.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -27,6 +28,12 @@ const router = createRouter({
       path: "/projects/:slug",
       name: "projects.show",
       component: AppShow,
+    },
+    //gestire rotte sbagliate e non valide
+    {
+      path: "/:pathMatch(.*)*",
+      name: "page404",
+      component: App404,
     },
   ],
 });
